bb3492064ef316c4c99a17af6801abad3abdef74
joao13-vitoras <70807079+joao13-vitoras@users.noreply.github.com>
2020-10-29 19:29:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let w = parseFloat(prompt('Digite um número:'));
+let x = parseFloat(prompt('Digite um número:'));
+let y = parseFloat(prompt('Digite um número:'));
+let z = parseFloat(prompt('Digite um número:'));
+let soma = w + x + y + z ;
+let media = soma / 4 ;
+
+alert (`A soma dos números foi ${soma}`);
+alert (`A média dos números foi ${media}`)
